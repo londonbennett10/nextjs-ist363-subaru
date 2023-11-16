f7e7ee4ff45fd8af9cae47f7015256027b19b67b
londonbennett10 <londonbennett@icloud.com>
2023-11-16 18:23:26 -0500

    attendance nov 16

--- pages/vehicles/[id].js
+++ pages/vehicles/[id].js
@@ -6,7 +6,7 @@ import { getVehicleBySlug , getAllVehicleSlugs } from "../../lib/api";
 //1 LALAl
 
 export async function getStaticPaths(){
-    const pathsArr = getAllVehicleSlugs();
+    const vehicles = await getAllVehicleSlugs();
     const paths = vehicles.map((vehicle) => {
         const { slug } = vehicle.node;
         return{
@@ -23,11 +23,11 @@ export async function getStaticPaths(){
 }
 
 
-
+//2
 
 
 export async function getStaticProps({ params }) {
-    const vehicleData = getVehicleBySlug(params.id); 
+    const vehicleData = await getVehicleBySlug(params.id); 
 
     return {
         props : {
@@ -37,12 +37,13 @@ export async function getStaticProps({ params }) {
 }
 
 
+//3
+
 
 const SingleVehiclePage = ({ vehicleData }) => {
-    const {model,price} = vehicleData;
+    const {title, slug} = vehicleData;
     return <Layout> 
-        <h1> {model} </h1>
-        <h2> ${price} </h2>
+        <h1> {title} </h1>
     </Layout>
 }
 
--- pages/vehicles/index.js
+++ pages/vehicles/index.js
@@ -18,11 +18,11 @@ const VehiclesPage = ({vehicles}) => {
         <h1>vehicles</h1>
         <ul>
             {vehicles.map((vehicle, index) => {
-                const{ title, slug} = vehicle.node;
+                const{title, slug} = vehicle.node;
                 return <li key={index}> 
                         <h3> {title} </h3>
                         <p> <Link href={`/vehicles/${slug}`}> Learn More </Link> </p>
-                     </li>
+                    </li>
             })}
         </ul>
     </Layout>
